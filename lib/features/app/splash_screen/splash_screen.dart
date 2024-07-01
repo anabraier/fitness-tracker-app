240eb2d406a95d7bf14aff0497a250609ea44a62
Carol Braier <carolbraier@macbook-pro-2.lan>
2024-06-30 22:38:15 -0400

    Add video player to splash screen and update video to fitnesstracker.mp4

--- lib/features/app/splash_screen/splash_screen.dart
+++ lib/features/app/splash_screen/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:video_player/video_player.dart';
 
 class SplashScreen extends StatefulWidget {
   final Widget? child;
@@ -10,27 +10,47 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late VideoPlayerController _controller;
+
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 3), () {
+    super.initState();
+    _controller = VideoPlayerController.asset('lib/videos/fitnesstracker.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+        _controller.setLooping(true);
+      });
+
+    Future.delayed(const Duration(seconds: 5), () {
       Navigator.pushAndRemoveUntil(
         context,
         MaterialPageRoute(builder: (context) => widget.child!),
-        (route) => false
+        (route) => false,
       );
     });
-    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Welcom to Flutter Firebase",
-          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold)
+    return Scaffold(
+      body: Container(
+        color: Colors.grey[200],
+        child: Center(
+          child: _controller.value.isInitialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : const CircularProgressIndicator(),
         ),
-      )
+      ),
     );
   }
 }
